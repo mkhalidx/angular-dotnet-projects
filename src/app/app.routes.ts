1a94486680a4e96bd547f8e94ac270ee6549ecd7
mkhalidx <khalid.26227@gmail.com>
2025-10-21 00:14:46 +0530

    Setup App Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'signin',
+    loadComponent: () =>
+      import('./components/signin/signin.component').then((m) => m.SigninComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./components/signup/signup.component').then((m) => m.SignupComponent),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./components/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+];
